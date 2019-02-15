ea9df5ff0847758a54efbc0b6ed15aa7a820896d
olympiawojcik <owojcik7@gmail.com>
2019-02-15 12:44:18 -0500

    create StarWarsList.js component, pass in state through props from our App to StarWarsList, test our class is working

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import StarWarsList from "./components/StarWarsList";
 
 class App extends Component {
   constructor() {
@@ -10,7 +12,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    this.getCharacters("https://swapi.co/api/people");
   }
 
   getCharacters = URL => {
@@ -33,6 +35,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <StarWarsList starwarsChars={this.state.starwarsChars} />
       </div>
     );
   }
